5d36160b299eec8f98d8f83c271965b89de6116d
Alexei <krisfragerfeed@gmail.com>
2020-09-10 20:11:06 +0300

    Checked and Unchecked

---
+++ lab2/task6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            firstFunc();
+            secondFunc();
+        }
+
+        static void firstFunc()
+        {
+            int f = checked(2147483647);
+            Console.WriteLine(f);
+        }
+
+        static void secondFunc()
+        {
+            int f = unchecked(2147483647 + 1);
+            Console.WriteLine(f);
+        }
+
+
+    }
+}
