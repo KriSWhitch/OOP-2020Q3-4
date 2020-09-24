ff37a94590e46aa031563d4f10f3747e66faaa73
Alexei <krisfragerfeed@gmail.com>
2020-09-24 07:15:36 +0300

    feat: complete task №1

--- lab4/lab4/Program.cs
+++ lab4/lab4/Program.cs
@@ -1,21 +1,52 @@
 ﻿using System;
+using System.Linq;
 
 namespace lab4
 {
 
     class List
     {
-        public int[] array;
+        public string[] array;
 
         public List()
         {
-            this.array = new int[] { };
+            this.array = new string[] { };
         }
 
-        public List(int[] array)
+        public List(string[] array)
         {
             this.array = array;
         }
+
+
+        public static bool operator ==(List c1, List c2)
+        {
+            return c1.array.SequenceEqual(c2.array);
+        }
+
+        public static bool operator !=(List c1, List c2)
+        {
+            return !(c1.array.SequenceEqual(c2.array));
+        }
+
+        public static List operator +(List c1, string value)
+        {
+            c1.AppendToStart(value);
+            return c1;
+        }
+
+        public static List operator --(List c1)
+        {
+            c1.RemoveFromStart();
+            return c1;
+        }
+
+        public static List operator *(List c1, List c2)
+        {
+            for (int i = 0; i < c2.array.Length; i++) c1.AppendToEnd(c2.array[i]);
+            return c1;
+        }
+
 
         public void DisplayListToConsole()
         {
@@ -35,9 +66,45 @@ namespace lab4
             Console.WriteLine($"Список: {result}");
         }
 
-        public void AppendToStart(int value)
+        public void IsEmpty()
         {
-            int[] result = new int[array.Length + 1];
+            if (array.Length == 0)
+            {
+                Console.WriteLine($"Список пуст!");
+            } else
+            {
+                Console.WriteLine($"Список содержит элементы!");
+            }
+        }
+
+        public void RepeatingElements()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        Console.WriteLine($"Элемент №{i + 1} совпадает с элементом №{j + 1}");
+                    }
+                }
+                
+            }
+        }
+
+        public void CapitalizedWords()
+        {
+            int result = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i].ToCharArray()[0].ToString() == array[i].ToCharArray()[0].ToString().ToUpper()) result++;
+            }
+            Console.WriteLine($"{result} слов начинается с большой буквы");
+        }
+
+        public void AppendToStart(string value)
+        {
+            string[] result = new string[array.Length + 1];
             result[0] = value;
             for (int i = 0; i < array.Length; i++)
                 result[i + 1] = array[i];
@@ -47,15 +114,15 @@ namespace lab4
 
         public void RemoveFromStart()
         {
-            int[] result = new int[array.Length - 1];
+            string[] result = new string[array.Length - 1];
             for (int i = 1; i < array.Length; i++)
                 result[i-1] = array[i];
             this.array = result;
         }
 
-        public void AppendToEnd(int value)
+        public void AppendToEnd(string value)
         {
-            int[] result = new int[array.Length + 1];
+            string[] result = new string[array.Length + 1];
             result[result.Length - 1] = value;
             for (int i = array.Length - 1; i >= 0; i--)
             {
@@ -66,7 +133,7 @@ namespace lab4
 
         public void RemoveFromEnd()
         {
-            int[] result = new int[array.Length - 1];
+            string[] result = new string[array.Length - 1];
             for (int i = 0; i < array.Length - 1; i++)
                 result[i] = array[i];
             this.array = result;
@@ -79,27 +146,29 @@ namespace lab4
     {
         static void Main(string[] args)
         {
-            int[] array = new int[] {1};
+            string[] array = new string[] { "1" };
             List fList = new List(array);
             fList.DisplayListToConsole();
-            List sList = new List(new int[] {5, 8, 9 });
+            List sList = new List(new string[] { "5", "8", "9" });
             sList.DisplayListToConsole();
             List tList = new List();
+            Console.WriteLine(tList != sList); // перегружена операция неравенства
+            tList.AppendToEnd("5"); tList.AppendToEnd("8"); tList.AppendToEnd("9");
+            Console.WriteLine(tList != sList);
+            Console.Write($"Список до использования перегруженной операции \"--\" : ");
             tList.DisplayListToConsole();
-            tList.AppendToEnd(5);
+            tList--;
             tList.DisplayListToConsole();
-            tList.AppendToStart(6);
+            Console.Write($"Список до использования перегруженной операции \"+\" : ");
             tList.DisplayListToConsole();
-            tList.AppendToEnd(7);
+            List result = tList + "el1";
+            result.DisplayListToConsole();
+            Console.WriteLine($"Списки до объединения: ");
+            sList.DisplayListToConsole();
             tList.DisplayListToConsole();
-            tList.AppendToStart(7);
-            tList.DisplayListToConsole();
-            tList.RemoveFromStart();
-            tList.DisplayListToConsole();
-            tList.RemoveFromEnd();
-            tList.DisplayListToConsole();
-            tList.RemoveFromEnd();
-            tList.DisplayListToConsole();
+            Console.WriteLine($"Список после объединения: ");
+            result = tList * sList;
+            result.DisplayListToConsole();
         }
     }
 }
