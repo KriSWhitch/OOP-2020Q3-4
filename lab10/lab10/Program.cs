8a539e77c9673fa1f61c1acb9a2a1b8d09a7e311
Alexei <krisfragerfeed@gmail.com>
2020-10-29 08:32:53 +0300

    feat: add task3

--- lab10/lab10/Program.cs
+++ lab10/lab10/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace lab10
 {
@@ -22,6 +24,8 @@ namespace lab10
     {
         static void Main(string[] args)
         {
+
+            // Задание 1
             ArrayList list = new ArrayList();
             list.Add(5); list.Add(6); list.Add(3); list.Add(6); list.Add(3);
             list.Add("string"); list.Add(new Student(18));
@@ -32,6 +36,8 @@ namespace lab10
             }
             Console.WriteLine($"Всего в колекции {list.Count} элементов");
             if (list.Contains("string")) Console.WriteLine($"list содержит \"string\" под индексом { list.IndexOf("string") }");
+
+            // Задание 2
             Dictionary<int, string> countries = new Dictionary<int, string>(5);
             countries.Add(1, "Belarus"); countries.Add(2, "Russia"); countries.Add(3, "Ukraine");
             countries.Add(4, "USA"); countries.Add(5, "France");
@@ -59,7 +65,44 @@ namespace lab10
             }
             if (countriesHash.Contains("Belarus")) Console.WriteLine($"countriesHash содержит элемент: \"Belarus\"");
 
+            // Задание 3
+            Dictionary<int, Flowers> flowers = new Dictionary<int, Flowers>(5);
+            Flowers flower1 = new Flowers("Мак");
+            Flowers flower2 = new Flowers("Алоэ");
+            Flowers flower3 = new Flowers("Роза");
+            Flowers flower4 = new Flowers("Ромашка");
+            Flowers flower5 = new Flowers("Пион");
+            flowers.Add(1, flower1);
+            flowers.Add(2, flower2);
+            flowers.Add(3, flower3);
+            flowers.Add(4, flower4);
+            flowers.Add(5, flower5);
+            foreach (KeyValuePair<int, Flowers> el in flowers)
+            {
+                Console.WriteLine(el.Value.name);
+            }
+            Console.Write($"Введите число элементов, которое вы хотите удалить с конца: ");
+            n = Int32.Parse(Console.ReadLine());
+            f = flowers.Count - n;
+            for (int i = flowers.Count; i > f; i--)
+            {
+                Console.WriteLine($"{flowers[i].name} удалён");
+                flowers.Remove(i);
+            }
+            Console.WriteLine($"Оставшиеся элементы: ");
+            foreach (KeyValuePair<int, Flowers> el in flowers)
+            {
+                Console.WriteLine(el.Value.name);
+            }
+            HashSet<Flowers> flowersHash = new HashSet<Flowers>(flowers.Count);
+            for (int i = 1; i <= countries.Count; i++)
+            {
+                flowersHash.Add(flowers[i]);
+            }
+            if (flowersHash.Contains(flower1)) Console.WriteLine($"flowersHash содержит элемент: \"flower1\"");
 
+            // Задание 4
+            ObservableCollection<Flowers> task4 = new ObservableCollection<Flowers>(); 
         }
     }
 }
---
+++ lab10/lab10/lab5.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab10
+{
+    abstract class Product
+    {
+        public string name;
+        public object[] elList;
+
+        public virtual void GetInfo()
+        {
+            Console.WriteLine($"Название продукта: {name}");
+        }
+
+
+        public Product()
+        {
+            this.name = "Без имени";
+        }
+
+        public Product(string name)
+        {
+            this.name = name;
+        }
+
+
+        public void AppendToEnd(object value)
+        {
+            object[] result = new object[elList.Length + 1];
+            result[result.Length - 1] = value;
+            for (int i = elList.Length - 1; i >= 0; i--)
+            {
+                result[i] = elList[i];
+            }
+            this.elList = result;
+        }
+
+        public void RemoveFromEnd()
+        {
+            object[] result = new object[elList.Length - 1];
+            for (int i = 0; i < elList.Length - 1; i++)
+                result[i] = elList[i];
+            this.elList = result;
+        }
+
+        public void DisplayListToConsole()
+        {
+            if (elList.Length == 0)
+            {
+                Console.WriteLine($"Список пуст!");
+                return;
+            }
+
+            string result = "(";
+            for (int i = 0; i < elList.Length; i++)
+            {
+                if (i == elList.Length - 1) result += $"{elList[i]}";
+                else result += $"{elList[i]}, ";
+
+            }
+            result += ")";
+            Console.WriteLine($"Список: {result}");
+        }
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"";
+        }
+    }
+
+    class Pastry : Product
+    {
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"";
+        }
+    }
+
+    class Goods : Product
+    {
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"";
+        }
+    }
+
+    class Flowers : Goods
+    {
+        public Flowers()
+        {
+            this.name = "Без имени";
+        }
+
+        public Flowers(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"Название цветов: {name}";
+        }
+    }
+
+    class Cake : Pastry
+    {
+        public Cake()
+        {
+            this.name = "Без имени";
+        }
+
+        public Cake(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"Название торта: {name}";
+        }
+    }
+
+    class Clock : Goods
+    {
+        public Clock()
+        {
+            this.name = "Без имени";
+        }
+
+        public Clock(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"Название часов: {name}";
+        }
+    }
+
+    // Задание 1. Переопределены стандартные методы
+    // Задание 2. Virtual -> Override
+    class Sweets : Goods
+    {
+        int id;
+
+        public Sweets()
+        {
+            this.name = "Без имени";
+            this.id = GetHashCode();
+        }
+
+        public Sweets(string name)
+        {
+            this.name = name;
+            this.id = GetHashCode();
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Название конфет: {name}");
+        }
+
+        public string GetType()
+        {
+            return $"Конфеты";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as Sweets;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return this.id.Equals(item.id);
+
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() + 17;
+        }
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"Название конфет: {name}";
+        }
+    }
+
+
+    //Задание 4. GetInfo как одноименный метод
+
+    interface IMovable
+    {
+        public void GetInfo()
+        {
+            Console.WriteLine("Hi!");
+        }
+    }
+
+    abstract class Move
+    {
+        public abstract void GetInfo();
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"";
+        }
+    }
+
+    class Run : Move, IMovable
+    {
+
+        void IMovable.GetInfo()
+        {
+            Console.WriteLine("Oh, hi Mark! from Interface");
+        }
+
+        public override void GetInfo()
+        {
+            Console.WriteLine("Oh, hi Mark! from Abstract Class");
+        }
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"";
+        }
+    }
+    //
+
+    class Printer
+    {
+        public void IAmPrinting(object someObj)
+        {
+            Console.WriteLine($"Тип объекта ( из IAmPrinting): {someObj.GetType()}\n" +
+                $"Использование ToString этого объекта: \n{someObj.ToString()}\n");
+        }
+    }
+
+}
