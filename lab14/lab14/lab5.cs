b5466fa725009b128abd87d40366689fa432a49b
Alexei <krisfragerfeed@gmail.com>
2020-11-14 10:13:17 +0300

    init: start lab14

---
+++ lab14/lab14/lab5.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Linq;
+
+namespace lab14
+{
+
+    abstract class Product
+    {
+        public string name;
+        public object[] elList;
+
+        public virtual void GetInfo()
+        {
+            Console.WriteLine($"Название продукта: {name}");
+        }
+
+
+        public Product()
+        {
+            this.name = "Без имени";
+        }
+
+        public Product(string name)
+        {
+            this.name = name;
+        }
+
+
+        public void AppendToEnd(object value)
+        {
+            object[] result = new object[elList.Length + 1];
+            result[result.Length - 1] = value;
+            for (int i = elList.Length - 1; i >= 0; i--)
+            {
+                result[i] = elList[i];
+            }
+            this.elList = result;
+        }
+
+        public void RemoveFromEnd()
+        {
+            object[] result = new object[elList.Length - 1];
+            for (int i = 0; i < elList.Length - 1; i++)
+                result[i] = elList[i];
+            this.elList = result;
+        }
+
+        public void DisplayListToConsole()
+        {
+            if (elList.Length == 0)
+            {
+                Console.WriteLine($"Список пуст!");
+                return;
+            }
+
+            string result = "(";
+            for (int i = 0; i < elList.Length; i++)
+            {
+                if (i == elList.Length - 1) result += $"{elList[i]}";
+                else result += $"{elList[i]}, ";
+
+            }
+            result += ")";
+            Console.WriteLine($"Список: {result}");
+        }
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"";
+        }
+    }
+
+    class Pastry : Product
+    {
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"";
+        }
+    }
+
+    class Goods : Product
+    {
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"";
+        }
+    }
+
+    class Flowers : Goods
+    {
+        public Flowers()
+        {
+            this.name = "Без имени";
+        }
+
+        public Flowers(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"Название цветов: {name}";
+        }
+    }
+
+    class Cake : Pastry
+    {
+        public Cake()
+        {
+            this.name = "Без имени";
+        }
+
+        public Cake(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"Название торта: {name}";
+        }
+    }
+
+    class Clock : Goods
+    {
+        public Clock()
+        {
+            this.name = "Без имени";
+        }
+
+        public Clock(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"Название часов: {name}";
+        }
+    }
+
+    // Задание 1. Переопределены стандартные методы
+    // Задание 2. Virtual -> Override
+    class Sweets : Goods
+    {
+        int id;
+
+        public Sweets()
+        {
+            this.name = "Без имени";
+            this.id = GetHashCode();
+        }
+
+        public Sweets(string name)
+        {
+            this.name = name;
+            this.id = GetHashCode();
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Название конфет: {name}");
+        }
+
+        public string GetType()
+        {
+            return $"Конфеты";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as Sweets;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return this.id.Equals(item.id);
+
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() + 17;
+        }
+
+        public override string ToString()
+        {
+            return $"Тип данного объекта: {GetType()}\n" +
+                $"ID: {GetHashCode()}\n" +
+                $"Название конфет: {name}";
+        }
+    }
+
+
+
+}
