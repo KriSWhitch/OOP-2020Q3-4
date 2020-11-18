fe65c89f948d3c99222e527a8b33530c112f49f9
Alexei <krisfragerfeed@gmail.com>
2020-11-18 09:27:23 +0300

    feat: add task1,2

--- lab15/lab15/Program.cs
+++ lab15/lab15/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace lab15
 {
@@ -6,7 +8,30 @@ namespace lab15
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Задание 1
+            Console.WriteLine($"Задание 1");
+            foreach (Process proc in Process.GetProcesses())
+            {
+                Console.WriteLine($"Запущенный процесс имеет {proc.Id} - ID, " +
+                    $"{proc.ProcessName} - Имя, {proc.BasePriority} - Приоритет, " +
+                    $"{proc.Responding} - текущее состояние"
+                );
+            }
+
+            // Задание 2
+            Console.WriteLine($"Задание 2");
+            AppDomain domain = AppDomain.CurrentDomain;
+            Console.WriteLine($"Имя: {domain.FriendlyName}, Директория: {domain.BaseDirectory}");
+            Console.WriteLine($"Все сборки:");
+            foreach ( Assembly el in domain.GetAssemblies())
+            {
+                Console.WriteLine($"Имя сборки: {el.GetName().Name}");
+            }
+            //создание и настройка домена
+            Assembly[] assembly = domain.GetAssemblies();
+            AppDomain newDomain = AppDomain.CreateDomain("NewDomain");//создаем новый домен
+            newDomain.Load(assembly[1].GetName().Name);//получаем имя сборки
+            AppDomain.Unload(newDomain);
         }
     }
 }
